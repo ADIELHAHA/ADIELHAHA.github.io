05413bff487d39f21360fc4ee3e9ab1795a3c4b2
ADIELHAHA <153005367+ADIELHAHA@users.noreply.github.com>
2023-12-06 19:42:17 +0900

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "HAHAHAHA, I love you too";
+  question.innerHTML = "HAHAHAHA, BAKLAAA";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
